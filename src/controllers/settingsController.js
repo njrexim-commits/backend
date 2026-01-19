b38d89419124c7c55332bf6c25b488f00cb9a1c1
clientfoster <basumgarianand109@gmail.com>
2026-01-19 19:00:40 +0530

    display fix

--- src/controllers/settingsController.js
+++ src/controllers/settingsController.js
@@ -19,21 +19,21 @@ export const updateSettings = asyncHandler(async (req, res) => {
     let settings = await Settings.findOne();
 
     if (settings) {
-        settings.siteName = req.body.siteName || settings.siteName;
-        settings.siteDescription = req.body.siteDescription || settings.siteDescription;
-        settings.contactEmail = req.body.contactEmail || settings.contactEmail;
-        settings.contactPhone = req.body.contactPhone || settings.contactPhone;
-        settings.alternatePhone = req.body.alternatePhone || settings.alternatePhone;
-        settings.address = req.body.address || settings.address;
-        settings.city = req.body.city || settings.city;
-        settings.state = req.body.state || settings.state;
-        settings.pincode = req.body.pincode || settings.pincode;
-        settings.country = req.body.country || settings.country;
-        settings.facebookUrl = req.body.facebookUrl || settings.facebookUrl;
-        settings.twitterUrl = req.body.twitterUrl || settings.twitterUrl;
-        settings.linkedinUrl = req.body.linkedinUrl || settings.linkedinUrl;
-        settings.instagramUrl = req.body.instagramUrl || settings.instagramUrl;
-        settings.ogImageUrl = req.body.ogImageUrl || settings.ogImageUrl;
+        settings.siteName = req.body.siteName !== undefined ? req.body.siteName : settings.siteName;
+        settings.siteDescription = req.body.siteDescription !== undefined ? req.body.siteDescription : settings.siteDescription;
+        settings.contactEmail = req.body.contactEmail !== undefined ? req.body.contactEmail : settings.contactEmail;
+        settings.contactPhone = req.body.contactPhone !== undefined ? req.body.contactPhone : settings.contactPhone;
+        settings.alternatePhone = req.body.alternatePhone !== undefined ? req.body.alternatePhone : settings.alternatePhone;
+        settings.address = req.body.address !== undefined ? req.body.address : settings.address;
+        settings.city = req.body.city !== undefined ? req.body.city : settings.city;
+        settings.state = req.body.state !== undefined ? req.body.state : settings.state;
+        settings.pincode = req.body.pincode !== undefined ? req.body.pincode : settings.pincode;
+        settings.country = req.body.country !== undefined ? req.body.country : settings.country;
+        settings.facebookUrl = req.body.facebookUrl !== undefined ? req.body.facebookUrl : settings.facebookUrl;
+        settings.twitterUrl = req.body.twitterUrl !== undefined ? req.body.twitterUrl : settings.twitterUrl;
+        settings.linkedinUrl = req.body.linkedinUrl !== undefined ? req.body.linkedinUrl : settings.linkedinUrl;
+        settings.instagramUrl = req.body.instagramUrl !== undefined ? req.body.instagramUrl : settings.instagramUrl;
+        settings.ogImageUrl = req.body.ogImageUrl !== undefined ? req.body.ogImageUrl : settings.ogImageUrl;
 
         const updatedSettings = await settings.save();
         res.json(updatedSettings);
